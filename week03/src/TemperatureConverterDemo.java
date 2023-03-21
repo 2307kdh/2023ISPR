fd3f16211c12d1d754e06438dae62a6bb981f736
ishs2307 <nac72_220409@gclass.ice.go.kr>
2023-03-21 20:36:23 +0900

    w3v2 Temperature Converter

--- week03/src/TemperatureConverterDemo.java
+++ week03/src/TemperatureConverterDemo.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class TemperatureConverterDemo {
     public static void main(String[] args) {
-        double f=0.0,c=0.0;
+        double f=0.0,c=0.0,k=0.0;
         Scanner scanner = new Scanner(System.in);
         // 1) 화씨에서 섭씨로 2) 섭씨에서 화씨로 3) 종료
         while(true){
-            System.out.print("-1)Celcius to Fahrenheit -2)Fahrenheit to Celcius -3)Exit : ");
+            System.out.print("-1)Celcius to Fahrenheit -2)Fahrenheit to Celcius -3)Celcius to Kelvin -4)Exit : ");
             int menu = scanner.nextInt();
             if(menu==1){
                 System.out.print("Enter temperature in Celcius: ");
@@ -20,11 +20,13 @@ public class TemperatureConverterDemo {
                 System.out.println(f+"°F is "+c+"°C");
             }
             else if(menu==3) {
-                System.out.println("탈주시도");
-                break;
+                System.out.print("Enter temperature in Fahrenheit: ");
+                c = scanner.nextDouble();
+                k= c+273.15;
+                System.out.println(c+"°C is "+k+"K");
             }
-            else if(menu==6974){
-                System.out.println("거유지혜");
+            else if(menu==4){
+                System.out.println("탈주시도");
                 break;
             }
             else{
