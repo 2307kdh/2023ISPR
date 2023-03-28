be0ac8677402bc1905cb5217b31abe18c14d4f0d
ishs2307 <nac72_220409@gclass.ice.go.kr>
2023-03-28 19:35:56 +0900

    w4v5

--- week03/src/TemperatureConverterDemo.java
+++ week03/src/TemperatureConverterDemo.java
@@ -9,30 +9,34 @@ public class TemperatureConverterDemo {
         while(true){
             System.out.print("-1)Celcius to Fahrenheit -2)Fahrenheit to Celcius -3)Celcius to Kelvin -4)Exit : ");
             String menu = scanner.nextLine();
-            if(menu.equals("1")){
-                System.out.print("Enter temperature in Celcius: ");
-                c=scanner.nextDouble();
-                f=(c*9.0/5.0)+32.0;
-                System.out.println(c+"°C is "+f+"°F");
-            }
-            else if(menu.equals("2")){
-                System.out.print("Enter temperature in Fahrenheit: ");
-                f = scanner.nextDouble();
-                c= (f-32.0)*(5.0/9.0);
-                System.out.println(f+"°F is "+c+"°C");
-            }
-            else if(menu.equals("3")) {
-                System.out.print("Enter temperature in Fahrenheit: ");
-                c = scanner.nextDouble();
-                k= c+273.15;
-                System.out.println(c+"°C is "+k+"K");
-            }
-            else if(menu.equals("4")){
-                System.out.println("탈주시도");
-                break;
-            }
-            else{
-                System.out.println("plz check your finger. Choose from 1, 2, 3");
+            if(menu.equals("4")){
+            System.out.println("탈주시도");
+            break;}
+            switch(menu) {
+
+                case "1":
+                    System.out.print("Enter temperature in Celcius: ");
+                    c = scanner.nextDouble();
+                    f = (c * 9.0 / 5.0) + 32.0;
+                    System.out.println(c + "°C is " + f + "°F");
+                    break;
+                case "2":
+                    System.out.print("Enter temperature in Fahrenheit: ");
+                    f = scanner.nextDouble();
+                    c = (f - 32.0) * (5.0 / 9.0);
+                    System.out.println(f + "°F is " + c + "°C");
+                    break;
+                case "3":
+                    System.out.print("Enter temperature in Fahrenheit: ");
+                    c = scanner.nextDouble();
+                    k = c + 273.15;
+                    System.out.println(c + "°C is " + k + "K");
+                    break;
+
+                default:
+                    System.out.println("plz check your finger. Choose from 1, 2, 3");
+                    break;
+
             }
         }
     }
