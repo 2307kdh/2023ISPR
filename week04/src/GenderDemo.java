4ee853ec8441edeaf8c1ff17749d25661a45d658
ishs2307 <nac72_220409@gclass.ice.go.kr>
2023-03-28 19:47:35 +0900

    w4v0 genderdemo

---
+++ week04/src/GenderDemo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GenderDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input ssn number(13 digits) : ");
+        String socialSecurityNumber = scanner.nextLine();
+        char gender = socialSecurityNumber.charAt(6);
+        switch (gender){
+            case '1':
+            case '3':
+                System.out.println("Male");
+                break;
+            case '2':
+            case '4':
+                System.out.println("Female");
+                break;
+            default:
+                System.out.println("plz check your finger. This isn't korean social security number");
+        }
+    }
+}
