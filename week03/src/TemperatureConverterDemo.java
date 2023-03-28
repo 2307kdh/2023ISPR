44d05517f55d920804019f1d4017da594f9ce4be
ishs2307 <nac72_220409@gclass.ice.go.kr>
2023-03-28 19:14:34 +0900

    temperature ver4

--- week03/src/TemperatureConverterDemo.java
+++ week03/src/TemperatureConverterDemo.java
@@ -8,26 +8,26 @@ public class TemperatureConverterDemo {
         // 1) 화씨에서 섭씨로 2) 섭씨에서 화씨로 3) 종료
         while(true){
             System.out.print("-1)Celcius to Fahrenheit -2)Fahrenheit to Celcius -3)Celcius to Kelvin -4)Exit : ");
-            int menu = scanner.nextInt();
-            if(menu==1){
+            String menu = scanner.nextLine();
+            if(menu.equals("1")){
                 System.out.print("Enter temperature in Celcius: ");
                 c=scanner.nextDouble();
                 f=(c*9.0/5.0)+32.0;
                 System.out.println(c+"°C is "+f+"°F");
             }
-            else if(menu==2){
+            else if(menu.equals("2")){
                 System.out.print("Enter temperature in Fahrenheit: ");
                 f = scanner.nextDouble();
                 c= (f-32.0)*(5.0/9.0);
                 System.out.println(f+"°F is "+c+"°C");
             }
-            else if(menu==3) {
+            else if(menu.equals("3")) {
                 System.out.print("Enter temperature in Fahrenheit: ");
                 c = scanner.nextDouble();
                 k= c+273.15;
                 System.out.println(c+"°C is "+k+"K");
             }
-            else if(menu==4){
+            else if(menu.equals("4")){
                 System.out.println("탈주시도");
                 break;
             }
