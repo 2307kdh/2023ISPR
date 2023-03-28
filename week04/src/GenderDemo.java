96e81b9d71b0eb26e623384b460fa49e43ffdb92
ishs2307 <nac72_220409@gclass.ice.go.kr>
2023-03-28 19:56:07 +0900

    w4v1genderdemo

--- week04/src/GenderDemo.java
+++ week04/src/GenderDemo.java
@@ -6,17 +6,11 @@ public class GenderDemo {
         System.out.print("Input ssn number(13 digits) : ");
         String socialSecurityNumber = scanner.nextLine();
         char gender = socialSecurityNumber.charAt(6);
-        switch (gender){
-            case '1':
-            case '3':
-                System.out.println("Male");
-                break;
-            case '2':
-            case '4':
-                System.out.println("Female");
-                break;
-            default:
-                System.out.println("plz check your finger. This isn't korean social security number");
-        }
+        String  genderstring = switch (gender){
+            case '1', '3'->"Male";
+            case '2', '4'->"Female";
+            default->"plz check your finger. This isn't korean social security number";
+        };
+        System.out.println(genderstring);
     }
 }
