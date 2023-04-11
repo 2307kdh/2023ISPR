a02494cf088e8caaf587a976e08fdfac1023c776
ishs2307 <nac72_220409@gclass.ice.go.kr>
2023-04-11 20:47:49 +0900

    week06 real last

---
+++ week06/src/kr/icehs/iscience/HighSchoolDemo.java
@@ -0,0 +1,8 @@
+package kr.icehs.iscience;
+import game.pokemon;
+public class HighSchoolDemo {
+    public static void main(String[] args) {
+        System.out.println("Incheon Science High School");
+        pokemon purin = new pokemon("퓨린",10,500);
+    }
+}
