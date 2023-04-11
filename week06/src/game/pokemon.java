29c72355afc2dab6afdfd47bb8d0eeafcf254a45
ishs2307 <nac72_220409@gclass.ice.go.kr>
2023-04-11 19:35:52 +0900

    week06 ver1

--- week06/src/game/pokemon.java
+++ week06/src/game/pokemon.java
@@ -1,9 +1,42 @@
 package game;
 
 public class pokemon {
-    String name;
-    int level;
-    int hp;
+    private String name;
+    private String hg;
+    private int level;
+
+    public String getHg() {
+        return hg;
+    }
+
+    public void setHg(String hg) {
+        this.hg = hg;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    private int hp;
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     void evolve(){
         hp+=10;
         level+=1;
--- week06/src/game/pokemongame.java
+++ week06/src/game/pokemongame.java
@@ -4,11 +4,19 @@ public class pokemongame {
     public static void main(String[] args) {
         pokemon pikachu = new pokemon();
         pokemon charizard = new pokemon();
-        pikachu.name = "피카츄";
-        charizard.name = "리자몽";
-        pikachu.level = 5;
-        charizard.level = 36;
+        pikachu.setName("피카츄");
+        charizard.setName("리자몽");
+        System.out.println(pikachu.getName());
+//        pikachu.name = "피카츄";
+//        charizard.name = "리자몽";
+//        pikachu.level = 5;
+//        charizard.level = 36;
+        pikachu.setLevel(5);
+        charizard.setLevel(36);
         pikachu.evolve();
+        charizard.evolve();
         pikachu.attack();
+        charizard.attack();
+
     }
 }
