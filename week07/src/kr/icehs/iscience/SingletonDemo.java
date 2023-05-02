ad7509c193bbf16524a1280f8034801603fbee3e
ishs2307 <nac72_220409@gclass.ice.go.kr>
2023-05-02 20:34:43 +0900

    week07 class singleton v0.2

--- week07/src/kr/icehs/iscience/SingletonDemo.java
+++ week07/src/kr/icehs/iscience/SingletonDemo.java
@@ -8,10 +8,20 @@ class Singleton{
     public static Singleton getInstance() {
         return s;
     }
+
+    @Override
+    public String toString() {
+        return "Singleton pattern object";
+    }
 }
 public class SingletonDemo {
     public static void main(String[] args) {
-        //Singleton s = new Singleton();//private access때문
         System.out.println(Singleton.getInstance().toString());
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        if(s1==s2)
+            System.out.println("same Object");
+        else
+            System.out.println("Different Object");
     }
 }
