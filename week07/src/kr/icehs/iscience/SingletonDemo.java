b3d3364b447b2b8f54269a25b6e4909eb8537982
ishs2307 <nac72_220409@gclass.ice.go.kr>
2023-05-02 20:24:39 +0900

    week07 class singleton v0.1

--- week07/src/kr/icehs/iscience/SingletonDemo.java
+++ week07/src/kr/icehs/iscience/SingletonDemo.java
@@ -1,12 +1,17 @@
 package kr.icehs.iscience;
 class Singleton{
+    private static Singleton s = new Singleton(); // unique object
     private Singleton() {
-        System.out.println("단 하나의 객체!");
+        System.out.println("Only One Object is Created!");
+    }
+
+    public static Singleton getInstance() {
+        return s;
     }
 }
 public class SingletonDemo {
     public static void main(String[] args) {
         //Singleton s = new Singleton();//private access때문
-
+        System.out.println(Singleton.getInstance().toString());
     }
 }
